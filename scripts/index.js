8d4e4b13c84113ff81fa233644eb0e488b02d24a
Tatyana Arb <nevarus@yandex.ru>
2023-03-10 12:42:12 +0300

    ПР4 - коммит №1стартовый коммит

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+//Блок управления popup:
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupOpenButtonElement = document.querySelector('.edit-button');
+const popupFormElement = document.querySelector('.popup__form');
+const inputName = popupElement.querySelector('#popup__text-name');
+const inputProf = popupElement.querySelector('#popup__text-prof');
+const profileTitleElement = document.querySelector('.profile__name');
+const profileTextElement = document.querySelector('.profile__prof');
+
+//Функция переключения отображения popup
+const toggleVisibilityPopup = function () {
+  popupElement.classList.toggle('popup__is-opened');
+};
+//Функция передает новые значения из input popup в секцию profile
+const savePopup = function (evt) {
+  evt.preventDefault();
+  profileTitleElement.textContent = inputName.value;
+  profileTextElement.textContent = inputProf.value;
+  toggleVisibilityPopup();
+};
+//Добавляем событие-клик к функции переключения отображения popup
+popupOpenButtonElement.addEventListener('click', toggleVisibilityPopup);
+popupCloseButtonElement.addEventListener('click', toggleVisibilityPopup);
+popupFormElement.addEventListener('submit', savePopup);
+//Завершен блок управления popup!
+
+//Блок управления лайками:
+const placeLikesArr = document.querySelectorAll('.place__likes');
+
+const activeLike = function (likeEvent) {
+  likeEvent.currentTarget.classList.toggle('place__likes-active');
+}
+
+placeLikesArr.forEach(function (like) {
+  like.addEventListener('click', activeLike);
+})
